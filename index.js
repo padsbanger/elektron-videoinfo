a50235f284b09ed27c2558b9a51fc7135cbde6c6
Michał Lach <kontakt@michal-lach.pl>
2019-11-14 11:11:26 +0100

    working todo

--- index.js
+++ index.js
@@ -1,9 +1,65 @@
 const electron = require("electron");
-const ffmpeg = require("fluent-ffmpeg");
 
-const { app, BrowserWindow, ipcMain } = electron;
+const { app, BrowserWindow, Menu, ipcMain } = electron;
 
 let mainWindow;
+
+function createAddWindow() {
+  addWindow = new BrowserWindow({
+    width: 400,
+    height: 400,
+    title: "Add New Todo",
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+  addWindow.loadURL(`file://${__dirname}/add.html`);
+  addWindow.on("closed", () => (addWindow = null));
+}
+
+ipcMain.on("todo:add", (event, todo) => {
+  console.log("yep");
+  mainWindow.webContents.send("todo:add", todo);
+  addWindow.close();
+});
+
+let menuTemplate = [
+  {
+    label: "File",
+    submenu: [
+      {
+        label: "New Todo",
+        click() {
+          createAddWindow();
+        }
+      },
+      {
+        label: "Quit",
+        accelerator: "Command+Q",
+        click() {
+          app.quit();
+        }
+      }
+    ]
+  }
+];
+
+if (process.env.NODE_ENV !== "production") {
+  menuTemplate.push({
+    label: "Extra",
+    submenu: [
+      {
+        label: "Toggle Developer Tools",
+        click(item, focusedWindow) {
+          focusedWindow.toggleDevTools();
+        }
+      },
+      {
+        role: "reload"
+      }
+    ]
+  });
+}
 
 app.on("ready", () => {
   mainWindow = new BrowserWindow({
@@ -12,11 +68,8 @@ app.on("ready", () => {
     }
   });
   mainWindow.loadURL(`file://${__dirname}/index.html`);
-  console.log("ready");
-});
+  mainWindow.on("closed", () => app.quit());
 
-ipcMain.on("video:submit", (event, path) => {
-  ffmpeg.ffprobe(path, (err, metadata) => {
-    mainWindow.webContents.send("video:metadata", metadata.format.duration);
-  });
+  const mainMenu = Menu.buildFromTemplate(menuTemplate);
+  Menu.setApplicationMenu(mainMenu);
 });
